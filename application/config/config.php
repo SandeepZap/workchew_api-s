9f870fb0160a5d2124854d9ecc896711e71e23b2
miamioh6 <will@drinkfly.com>
2017-11-17 17:14:49 +0530

    commit config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/workchew';
-//$config['base_url'] = 'workchew.zapbuild.com';
+//$config['base_url'] = 'http://localhost/workchew';
+$config['base_url'] = 'http://workchew.zapbuild.com';
 
 /*
 |--------------------------------------------------------------------------
