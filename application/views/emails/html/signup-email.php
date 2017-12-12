ee85a05711bd1abebe1b015f468a825128f4443d
miamioh6 <will@drinkfly.com>
2017-12-12 12:43:20 +0530

    commit changes for wecome email template

--- application/views/emails/html/signup-email.php
+++ application/views/emails/html/signup-email.php
@@ -1,3 +1,7 @@
 <p style="font-size:30px; color:#555555; font-weight:300; margin:70px 0 5px;">Hello <?php echo ucfirst($first_name) ?></p>
-<p style="font-size:20px; color:#555555; font-weight:300">Thanks for the resgiteration.</b></p>
+<p style="font-size:20px; color:#555555; font-weight:300">WELCOME TO WORKCHEW</b></p>
+<p style="font-size:20px; color:#555555; font-weight:300">We’re so happy to have you onboard!</b></p>
+<p style="font-size:20px; color:#555555; font-weight:300">We are thrilled that you have joined us! Congratulations.</b></p>
+<p style="font-size:20px; color:#555555; font-weight:300">You signed up by creating the email <?php echo $email?> and your chosen <?php echo $password?>, so you can use that to login again later.</b></p>
+					
 
