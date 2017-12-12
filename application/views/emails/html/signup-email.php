d3d1bf6c4fbe2c16965f04aa6160f72ec52c13b5
miamioh6 <will@drinkfly.com>
2017-12-12 12:00:15 +0530

    add new sign up email template

---
+++ application/views/emails/html/signup-email.php
@@ -0,0 +1,3 @@
+<p style="font-size:30px; color:#555555; font-weight:300; margin:70px 0 5px;">Hello <?php echo ucfirst($first_name) ?></p>
+<p style="font-size:20px; color:#555555; font-weight:300">Thanks for the resgiteration.</b></p>
+
